0f5faf6b04e7f8df72f7328bd787b33435368344
Nicolai Pagh <nicolaipagh@protonmail.com>
2019-03-19 12:42:20 +0100

    actual initial commit

---
+++ dweem-monitor/dweem-monitor/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="dweem_monitor.MvcApplication" Language="C#" %>
---
+++ dweem-monitor/dweem-monitor/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(dweem_monitor.Startup))]
+namespace dweem_monitor
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
