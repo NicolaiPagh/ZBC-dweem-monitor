be1ef89c8125cc6e0fa9abab3807f9903788e38d
NicolaiPagh <nicolaipagh@protonmail.com>
2019-03-21 21:15:09 +0100

    tweaks to performance monitors in GetInfo.cs

--- dweem-monitor/dweem-monitor/dweem_mon/GetInfo.cs
+++ dweem-monitor/dweem-monitor/dweem_mon/GetInfo.cs
@@ -8,17 +8,26 @@ namespace dweem_monitor
 {
     public class Monitor
     {
-        public static string getCurrentCpuUsage()
+        public static int getCurrentCpuUsage()
         {
             PerformanceCounter cpuCounter = new PerformanceCounter("Processor", "% Processor Time", "_Total");
             cpuCounter.NextValue();
-            return cpuCounter.NextValue() + "%";
+            //return cpu usage as an int in percent
+            return Convert.ToInt32(cpuCounter.NextValue());
         }
 
-        public static string getAvailableRAM()
+        public static int getAvailableRAM()
         {
             PerformanceCounter ramCounter = new PerformanceCounter("Memory", "Available MBytes");
-            return ramCounter.NextValue() + "MB";
+            //return ram available as an int in MB
+            return Convert.ToInt32(ramCounter.NextValue());
+        }
+
+        public static float getCommitedRAM()
+        {
+            PerformanceCounter ramCounter = new PerformanceCounter("Memory", "% Committed Bytes In Use");
+            //return ram available as an int in MB
+            return ramCounter.NextValue();
         }
     }
 }
