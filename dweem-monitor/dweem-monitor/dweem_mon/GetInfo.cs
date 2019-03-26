4fee1a129b4a6d7a8884f9567c623f7d01a94034
Nicolai Pagh <nicolaipagh@protonmail.com>
2019-03-26 12:28:05 +0100

    added a bunch more performance counters and a couple changes to current ones

--- dweem-monitor/dweem-monitor/dweem_mon/GetInfo.cs
+++ dweem-monitor/dweem-monitor/dweem_mon/GetInfo.cs
@@ -29,11 +29,32 @@ namespace dweem_monitor
             //return ram available as an int in MB
             return ramCounter.NextValue();
         }
-        public static float getNetworkBandwidth()
+        public static float getNetworkBandwidthMB()
         {
             PerformanceCounter netBandwidthCounter = new PerformanceCounter("Network Interface", "Current Bandwidth", "JMicron PCI Express Gigabit Ethernet Adapter");
             //return ram available as an int in MB
             return netBandwidthCounter.NextValue();
+        }
+        public static float getDiskAvgReadBytes()
+        {
+            PerformanceCounter diskAvgReadCounter = new PerformanceCounter("PhysicalDisk", "Avg. Disk Bytes/Read", "_Total");
+            return diskAvgReadCounter.NextValue();
+        }
+        public static float getDiskAvgWriteBytes()
+        {
+            PerformanceCounter diskAvgWriteCounter = new PerformanceCounter("PhysicalDisk", "Avg. Disk Bytes/Write", "_Total");
+            return diskAvgWriteCounter.NextValue();
+        }
+        public static float getDiskAvgThroughput()
+        {
+            PerformanceCounter diskAvgThroughput = new PerformanceCounter("PhysicalDisk", "Avg. Disk Bytes/Transfer", "_Total");
+            return diskAvgThroughput.NextValue();
+        }
+        public static float getSysUpTime()
+        {
+            PerformanceCounter sysUpTime = new PerformanceCounter("System", "System Up Time");
+            //gets system up time in miliseconds
+            return sysUpTime.NextValue();
         }
     }
 }
